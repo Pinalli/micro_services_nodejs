db5fa370ec1303f01e1d69e4120505681a5c0efd
Alberto Rocha Pinalli <betopinalli@gmail.com>
2022-12-15 11:58:01 -0300

    tests

---
+++ notification-service/src/application/entities/notification.spec.ts
@@ -0,0 +1,23 @@
+import { Notification } from './notification';
+import { Content } from './content';
+
+
+
+describe('Notification', () => {
+  it('should be able to create a notification content', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+      createAt: new Date(),
+    });
+
+// // test('should be able to create a notification', () => {
+// //     const notification = new Notification({
+// //       content: new Content('Nova solicitação de amizade'),
+// //       category: 'social',
+// //       recipientId: 'example-recipient-id',
+// //     });
+
+// //     expect(notification).toBeTruthy();
+// //   });
